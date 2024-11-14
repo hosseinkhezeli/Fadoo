486b33d430f3752d8d31af213540b27e20e035d0
Hossein Khezeli <123948444+hosseinkhezeli@users.noreply.github.com>
2024-11-14 16:51:32 +0330

    feat:add videography page work in progress

--- src/app/(sections)/Videography.tsx
+++ src/app/(sections)/Videography.tsx
@@ -1,7 +1,10 @@
 import { Container } from "@/components/Container";
+import { MountainsBG } from "./components/MountainsBG";
 
-export function Videography(){
-    return(
-      <Container id={'videography'}>-1</Container>
-    )
+export function Videography() {
+  return (
+    <Container id={"videography"}>
+      <MountainsBG />
+    </Container>
+  );
 }
